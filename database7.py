e8d8a946b8b4be637f25fc0bdb10ec8347bf8121
AshishSheoran08 <102245188+AshishSheoran08@users.noreply.github.com>
2022-03-23 22:03:44 +0530

    Update database7.py

--- database7.py
+++ database7.py
@@ -4,6 +4,24 @@ conn = sqlite3.connect('moviestable.db')
 
 cursor = conn.cursor()
 
+cursor.execute(""" CREATE TABLE movie (
+	movie_name text,
+	actor_name text,
+	actress_name text,
+	year_released int,
+	director_name text
+	)""")
+
+many_movie = 	[
+			('Amazing Spider Man', 'Andrew Garfield', 'Emma Stone', '2012', 'Marc Webb'),
+			('Avengers Endgame', 'Robert', 'Scarlett Johnson', '2019', 'Anthony Russo'),
+			('RRR', 'Ram Charan', 'Alia Bhatt', '2022', 'SS Rajamouli'),
+			('Kashmir Files', 'Darshan Kumar', 'Pallavi Joshi', '2022', 'Vivek Agnihotri'),
+			('Student of the year', 'Siddarth Malhotra', 'Alia Bhatt', '2016', 'Karan Johar'),
+		]
+
+cursor.executemany("INSERT INTO movie VALUES (?,?,?,?,?)", many_movie)
+
 cursor.execute("SELECT * FROM movie")
 
 items = cursor.fetchall()
